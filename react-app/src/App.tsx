c19637890c893caf66d3892da9f93718bf785ec7
fallou-mbengue <falloumbengue96@gmail.com>
2023-04-05 12:49:10 +0000

    sharing state between compoenent

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,46 +1,14 @@
-import ListGroup from "./components/ListGroup";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
 import { useState } from "react";
-import produce from "immer";
+import Cart from "./components/Cart";
+import NavBar from "./components/NavBar";
 
 function App() {
-  const [bugs, setBugs] = useState([
-    { id: 1, title: "bug 1", fixed: false },
-    { id: 2, title: "bug 2", fixed: false },
-  ]);
-
-  const handleClick = () => {
-    // update
-    // setBugs(bugs.map(bug => bug.id === 1 ? {...bugs, fixed:true} : bug))
-
-    // use immer
-    setBugs(
-      produce((draft) => {
-        const bug = draft.find((bug) => bug.id === 1);
-        if (bug) bug.fixed = true;
-      })
-    );
-  };
-
+  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
   return (
-    <>
-      {/* <ListGroup items={items} title="List" onSelectItem={handleSelectItem} />
-      <Button color="secondary" onClick={() => setAlertVisible(true)}>
-        My button
-      </Button>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisible(false)}>Show alert</Alert>
-      )} */}
-      <div>
-        {bugs.map((bug) => (
-          <p key={bug.id}>
-            {bug.title} {bug.fixed ? "fixed" : "New"}
-          </p>
-        ))}
-        <button onClick={handleClick}>Click Me</button>
-      </div>
-    </>
+    <div>
+      <NavBar cartItemsCount={cartItems.length} />
+      <Cart onClear={() => setCartItems([])} cartItems={cartItems} />
+    </div>
   );
 }
 
