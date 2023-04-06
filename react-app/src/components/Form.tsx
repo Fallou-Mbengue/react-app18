f925494c0cdbfaca903ea5866b684d2b530c3818
fallou-mbengue <falloumbengue96@gmail.com>
2023-04-06 11:14:59 +0000

    validation

--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -1,8 +1,16 @@
-import React, { FormEvent, useRef, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 
+interface FormData {
+  name: string;
+  age: number;
+}
+
 const Form = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
   const onSubmit = (data: FieldValues) => console.log(data);
 
   return (
@@ -12,22 +20,34 @@ const Form = () => {
           Name
         </label>
         <input
-          {...register("name")}
+          {...register("name", { required: true, minLength: 5 })}
           id="name"
           type="text"
           className="form-control"
         />
+        {errors.name?.type === "required" && (
+          <p className="text-center text-danger mt-2">Nom requise</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-center text-danger mt-2">Minimum 5 caractèère</p>
+        )}
       </div>
       <div className="mb-3 col-6 mx-auto">
         <label htmlFor="age" className="form-label">
           Age
         </label>
         <input
-          {...register("age")}
+          {...register("age", { required: true, minLength: 2 })}
           id="age"
           type="number"
           className="form-control"
         />
+        {errors.age?.type === "required" && (
+          <p className="text-center text-danger">Age requise</p>
+        )}
+        {errors.age?.type === "minLength" && (
+          <p className="text-center text-danger">min 2 chiffre</p>
+        )}
       </div>
       <div className="text-center">
         <button className="btn btn-primary" type="submit">
